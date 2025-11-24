04cfea259cf278f6cb97dbb0ccc0a450660ba955
Aravindh MS <aravindhms1@gmail.com>
2025-11-24 21:42:24 +0530

    Refactor typing animation and fix CSS corruption
    - Moved typing animation from name to subtitle with cycling words
    - Implemented JavaScript TypeWriter class for dynamic text animation
    - Fixed cursor appearance using pseudo-element instead of border
    - Restored corrupted CSS file with all Hero section styles
    - Updated Family section with language information

--- js/script.js
+++ js/script.js
@@ -166,4 +166,68 @@ document.addEventListener('DOMContentLoaded', () => {
 
     initParticles();
     animateParticles();
+
+    // Typing Animation
+    class TypeWriter {
+        constructor(txtElement, words, wait = 3000) {
+            this.txtElement = txtElement;
+            this.words = words;
+            this.txt = '';
+            this.wordIndex = 0;
+            this.wait = parseInt(wait, 10);
+            this.type();
+            this.isDeleting = false;
+        }
+
+        type() {
+            // Current index of word
+            const current = this.wordIndex % this.words.length;
+            // Get full text of current word
+            const fullTxt = this.words[current];
+
+            // Check if deleting
+            if (this.isDeleting) {
+                // Remove char
+                this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+                // Add char
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
+
+            // Insert txt into element
+            this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+            // Initial Type Speed
+            let typeSpeed = 100;
+
+            if (this.isDeleting) {
+                typeSpeed /= 2;
+            }
+
+            // If word is complete
+            if (!this.isDeleting && this.txt === fullTxt) {
+                // Make pause at end
+                typeSpeed = this.wait;
+                // Set delete to true
+                this.isDeleting = true;
+            } else if (this.isDeleting && this.txt === '') {
+                this.isDeleting = false;
+                // Move to next word
+                this.wordIndex++;
+                // Pause before start typing
+                typeSpeed = 500;
+            }
+
+            setTimeout(() => this.type(), typeSpeed);
+        }
+    }
+
+    // Init TypeWriter
+    const txtElement = document.querySelector('.typing-text');
+    const words = ["Engineer", "Tech Enthusiast", "Problem-Solving Geek", "Hobby Photographer", "Gadget Freak", "Calm Performer", "Automation Lover", "Cloud Curious"];
+    const wait = 2000;
+
+    if (txtElement) {
+        new TypeWriter(txtElement, words, wait);
+    }
 });
